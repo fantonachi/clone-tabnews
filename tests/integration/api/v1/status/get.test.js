b3c52813bcc4bb401ed008798ecbcaeb300c6da5
Felippe Antonachi <37147352+fantonachi@users.noreply.github.com>
2025-03-12 20:05:18 +0000

    add /api/v1/status `endpoint`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,9 @@
-test("GET para api/v1/status deve retornar 200", async () => {
+test("GET to api/v1/status should be 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+});
+
+test("GET to ExtracaoSei should be 200", async () => {
+  const response = await fetch("https://extracaosei.4mine.com.br:8443");
   expect(response.status).toBe(200);
 });
